a470cdacf0cb052b928f0fec8a5e2dc74a607992
dugite-code <dugite-code@peekread.info>
2019-06-04 11:33:08 +0800

    Options Compatability

---
+++ src/chrome/content/preferences.js
@@ -0,0 +1,7 @@
+Preferences.addAll([
+	{ id: "extensions.lookout.attach_raw_mapi", type: "bool" },
+	{ id: "extensions.lookout.direct_to_calendar", type: "bool" },
+  { id: "extensions.lookout.disable_filename_character_set", type: "bool" },
+  { id: "extensions.lookout.strict_contenttype", type: "bool" },
+  { id: "extensions.lookout.debug_enable", type: "bool" },
+]);
