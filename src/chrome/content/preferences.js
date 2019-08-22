79fbeac2416b78f7f519f0f713711ef9294e876f
dugite-code <dugite-code@peekread.info>
2019-08-22 09:51:10 +0800

    Preference fixes

--- src/chrome/content/preferences.js
+++ src/chrome/content/preferences.js
@@ -2,6 +2,7 @@ Preferences.addAll([
 	{ id: "extensions.lookout.attach_raw_mapi", type: "bool" },
 	{ id: "extensions.lookout.direct_to_calendar", type: "bool" },
   { id: "extensions.lookout.disable_filename_character_set", type: "bool" },
+  { id: "extensions.lookout.remove_winmail_dat", type: "bool" },
   { id: "extensions.lookout.strict_contenttype", type: "bool" },
   { id: "extensions.lookout.debug_enable", type: "bool" },
 ]);
