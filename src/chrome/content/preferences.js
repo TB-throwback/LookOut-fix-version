a61088b5f9622ff422e0e04763ddc46c4a2432de
dugite-code <dugite-code@peekread.info>
2019-08-30 15:31:09 +0800

    Fix #42

--- src/chrome/content/preferences.js
+++ src/chrome/content/preferences.js
@@ -4,5 +4,5 @@ Preferences.addAll([
   { id: "extensions.lookout.disable_filename_character_set", type: "bool" },
   { id: "extensions.lookout.remove_winmail_dat", type: "bool" },
   { id: "extensions.lookout.strict_contenttype", type: "bool" },
-  { id: "extensions.lookout.debug_enable", type: "bool" },
+  { id: "extensions.lookout.debug_enabled", type: "bool" },
 ]);
