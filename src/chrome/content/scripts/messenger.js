79c0e4579f956b3fff5ccb2438a7ae2ed80fa5d9
dugite-code <dugite-code@users.noreply.github.com>
2021-01-12 11:04:21 +0800

    Non-English parsing and HU specific fixes

--- src/chrome/content/scripts/messenger.js
+++ src/chrome/content/scripts/messenger.js
@@ -11,7 +11,7 @@ function onLoad(activatedWhileWindowOpen) {
   WL.injectCSS("resource://lookout/skin/lookout.css");
   WL.injectElements(`
     <menupopup id="taskPopup">
-      <menuitem id="lookout-settings" label="&lookout.label;" oncommand="lookout.openSettings();" insertbefore="prefSep" class="menu-iconic lookout-icon menuitem-iconic" />
+      <menuitem id="lookout-settings" label="lookout" oncommand="lookout.openSettings();" insertbefore="prefSep" class="menu-iconic lookout-icon menuitem-iconic" />
     </menupopup>`);
 
   window.LookoutLoad();
