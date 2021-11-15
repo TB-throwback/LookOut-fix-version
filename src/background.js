f11650adcb916f976daffe6f9cb8256df9677b8c
John Bieling <john.bieling@gmx.de>
2021-11-15 09:10:20 +0100

    fix #79

--- src/background.js
+++ src/background.js
@@ -1,22 +1,24 @@
-(async () => {
+browser.SessionRestore.onStartupSessionRestore.addListener(main);
 
-messenger.WindowListener.registerDefaultPrefs("defaults/preferences/lookout.js");
-messenger.WindowListener.registerChromeUrl([
-  ["content", "lookout",            "chrome/content/"],
-  ["locale",  "lookout", "en-US",   "chrome/locale/en-US/"],
-  ["locale",  "lookout", "hu",      "chrome/locale/hu/"],
-  ["locale",  "lookout", "de",      "chrome/locale/de/"],
-  ["locale",  "lookout", "ja",      "chrome/locale/ja/"],
-  ["locale",  "lookout", "nl",      "chrome/locale/nl/"]
-]);
-messenger.WindowListener.registerOptionsPage("chrome://lookout/content/options.xhtml");
+function main() {
+  browser.SessionRestore.onStartupSessionRestore.removeListener(main);
 
-messenger.WindowListener.registerWindow(
+  messenger.WindowListener.registerDefaultPrefs("defaults/preferences/lookout.js");
+  messenger.WindowListener.registerChromeUrl([
+    ["content", "lookout", "chrome/content/"],
+    ["locale", "lookout", "en-US", "chrome/locale/en-US/"],
+    ["locale", "lookout", "hu", "chrome/locale/hu/"],
+    ["locale", "lookout", "de", "chrome/locale/de/"],
+    ["locale", "lookout", "ja", "chrome/locale/ja/"],
+    ["locale", "lookout", "nl", "chrome/locale/nl/"]
+  ]);
+  messenger.WindowListener.registerOptionsPage("chrome://lookout/content/options.xhtml");
+
+  messenger.WindowListener.registerWindow(
     "chrome://messenger/content/messenger.xhtml",
     "chrome://lookout/content/scripts/messenger.js");
-messenger.WindowListener.registerWindow(
+  messenger.WindowListener.registerWindow(
     "chrome://messenger/content/messageWindow.xhtml",
     "chrome://lookout/content/scripts/messenger.js");
-messenger.WindowListener.startListening();
-
-})();
+  messenger.WindowListener.startListening();
+}
