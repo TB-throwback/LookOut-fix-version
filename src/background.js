1e1c28c136c185653e5131e8c2f132bd9cc09798
John Bieling <john.bieling@gmx.de>
2022-06-28 11:30:58 +0200

    Fix race conditions

--- src/background.js
+++ src/background.js
@@ -1,3 +1,34 @@
+async function getThunderbirdVersion() {
+  let browserInfo = await messenger.runtime.getBrowserInfo();
+  let parts = browserInfo.version.split(".");
+  return {
+    major: parseInt(parts[0]),
+    minor: parseInt(parts[1]),
+    revision: parts.length > 2 ? parseInt(parts[2]) : 0,
+  }
+}
+
+async function main() {
+  var majorVersion = await getThunderbirdVersion();
+  
+  // Must delay startup for issue #79
+  // Thunderbird 102 does this by default.
+  let startupDelay;
+  if (majorVersion.major < 92) {
+    startupDelay = await new Promise(async (resolve) => {
+      const restoreListener = (window, state = true) => {
+        browser.SessionRestore.onStartupSessionRestore.removeListener(restoreListener);
+        resolve(state);
+      }
+      browser.SessionRestore.onStartupSessionRestore.addListener(restoreListener);
+  
+      let isRestored = await browser.SessionRestore.isRestored();
+      if (isRestored) {
+        restoreListener(null, false);
+      }
+    });
+  }
+  
   messenger.WindowListener.registerDefaultPrefs("defaults/preferences/lookout.js");
   messenger.WindowListener.registerChromeUrl([
     ["content", "lookout", "chrome/content/"],
@@ -16,3 +47,6 @@
     "chrome://messenger/content/messageWindow.xhtml",
     "chrome://lookout/content/scripts/messenger.js");
   messenger.WindowListener.startListening();
+}
+
+main();
